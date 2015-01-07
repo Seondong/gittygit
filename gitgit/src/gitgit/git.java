162629b57e4dec845fea6f5861acb31d91de5b10
Seondong <sdkim0211@gmail.com>
2015-01-07 22:01:52 +0900

    Completed displaying data in a object

--- gitgit/src/gitgit/git.java
+++ gitgit/src/gitgit/git.java
@@ -1,13 +1,18 @@
 package gitgit;
 
 public class git {
+	
+	int data[] = {0, 4, 42};
 	public void laugh(){
 		System.out.println("Yeah");
 	}
 	
 	public void display(){
+		System.out.println("Data : ");
 		for(int i=0; i<3; i++){
+			System.out.println("Next value:" + data[i]);
 			System.out.println("Ho");
+			
 		}
 	}
 }
