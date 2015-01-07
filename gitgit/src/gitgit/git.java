d4a32af0ecdd4e222027ca7829105e605eb9bbb2
Seondong <sdkim0211@gmail.com>
2015-01-07 18:11:16 +0900

    GittyGit First ver

---
+++ gitgit/src/gitgit/git.java
@@ -0,0 +1,7 @@
+package gitgit;
+
+public class git {
+	public void laugh(){
+		System.out.println("Yeah");
+	}
+}
---
+++ gitgit/src/gitgit/main.java
@@ -0,0 +1,8 @@
+package gitgit;
+
+public class main {
+	public static void main(String args[]){
+		git gitagent = new git();
+		gitagent.laugh();
+	}
+}
